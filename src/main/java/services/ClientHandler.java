32e597fe8f7c4e1c5054a6f616bdddf0445e9c48
Zeben <zebenzuilopezconde@gmail.com>
2024-01-24 20:41:58 +0000

    commit: envio de datos y creación de .csv

--- src/main/java/services/ClientHandler.java
+++ src/main/java/services/ClientHandler.java
@@ -1,55 +1,70 @@
 package services;
 
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
 import java.net.Socket;
-import java.net.SocketException;
 import java.util.List;
+
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-
 public class ClientHandler extends Thread {
-    private Socket clientSocket;
+    private final Socket clientSocket;
 
+    // Constructor que acepta un socket como parámetro
     public ClientHandler(Socket socket) {
         this.clientSocket = socket;
     }
 
     @Override
     public void run() {
+        BufferedReader inFromClient = null;
         try {
-            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            DataOutputStream outToClient = new DataOutputStream(clientSocket.getOutputStream());
-
+            // Crear un BufferedReader para leer la entrada del cliente
+            inFromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             // Leer la entrada completa del cliente
             String line;
-            try {
-                while ((line = inFromClient.readLine()) != null) {
-                    StringBuilder sb = new StringBuilder();
-                    sb.append(line);
-                    String clientInput = sb.toString();
 
-                    // Deserializar la entrada en una lista
-                    Gson gson = new Gson();
-                    Type listType = new TypeToken<List<String>>(){}.getType();
-                    List<String> datosCliente = gson.fromJson(clientInput, listType);
+            while ((line = inFromClient.readLine()) != null) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(line);
+                String clientInput = sb.toString();
 
-                    // Imprimir los datos del cliente
-                    System.out.println("Datos recibidos del cliente: " + datosCliente);
+                // Deserializar la entrada en una lista de objetos
+                Gson gson = new Gson();
+                Type listType = new TypeToken<List<Object>>() {
+                }.getType();
+                List<Object> datosCliente = gson.fromJson(clientInput, listType);
+
+                // Imprimir los datos del cliente
+                System.out.println("Datos recibidos del cliente: " + datosCliente);
+
+                // Guardar datosCliente en un archivo CSV
+                try (FileWriter writer = new FileWriter("datosCliente.csv", true)) {
+                    for (Object dato : datosCliente) {
+                        // Reemplazar las comas con punto y coma
+//                        String processedDato = dato.toString().replace(",", ".");
+//                        writer.append(processedDato).append(",");
+                        writer.append(dato.toString()).append(",");
+                    }
+                    writer.append("\n");
+                } catch (IOException e) {
+                    System.out.println("Error al escribir en el archivo CSV");
                 }
-            } catch (SocketException e) {
-                System.out.println("No se puede leer la entrada del cliente");
-                return;
             }
 
-            // Hacer algo con la lista...
-
         } catch (IOException e) {
-            throw new RuntimeException("Error handling client communication", e);
+            System.out.println("Error al leer la entrada del cliente");
+        } finally {
+            try {
+                // Cerrar el socket del cliente
+                clientSocket.close();
+            } catch (IOException e) {
+                System.out.println("Cliente desconectado");
+            }
         }
     }
 }
--- src/main/java/services/Conexion.java
+++ src/main/java/services/Conexion.java
@@ -1,27 +1,22 @@
 package services;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Conexion {
     public void conectar() throws IOException {
-        String respuesta = "Conectado al server";
         int port = 6789;
         ServerSocket serverSocket = new ServerSocket(port);
 
         while (true) {
             try {
                 Socket connectionSocket = serverSocket.accept();
+//                System.out.println("Conectado al cliente");
                 new ClientHandler(connectionSocket).start();
-                System.out.println("Conectado al cliente");
             } catch (IOException e) {
                 System.out.println("Error al conectar al cliente");
             }
         }
-
     }
 }
